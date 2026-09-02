5eca6112f6b6049ccb1d0d6e3af3e26ede547f1f
Mariamawit Messay <maramessay655@gmail.com>
2026-09-02 17:08:07 +0300

    Update CORS_ALLOWED_ORIGINS for production
    Removed local CORS origins and added production URL.

--- config/settings.py
+++ config/settings.py
@@ -113,8 +113,7 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "https://megeb-plus-web.vercel.app",
 ]
 
 AUTHENTICATION_BACKENDS = [
